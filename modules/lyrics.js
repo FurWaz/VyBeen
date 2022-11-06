ff2826ce48f695019286464561aba6a34ad11a6d
FurWaz <fur.waz06@gmail.com>
2022-11-06 15:22:25 +0100

    improved lyrics finding

--- modules/lyrics.js
+++ modules/lyrics.js
@@ -6,8 +6,18 @@ function lyrics(artist, song) {
         artist = cleanString(artist);
         song = cleanString(song);
         const urlTemplate = "https://www.musixmatch.com/lyrics/{ARTIST}/{SONG}";
-        const link = urlTemplate.replace("{ARTIST}", artist).replace("{SONG}", song);
-        resolve(findInPage(link));
+        const links = [
+            urlTemplate.replace("{ARTIST}", artist).replace("{SONG}", song),
+            urlTemplate.replace("{ARTIST}", artist+"-5").replace("{SONG}", song)
+        ];
+        let index = 0;
+        function tryLink() {
+            findInPage(links[index]).then(resolve).catch(() => {
+                index++;
+                if (index < links.length) tryLink();
+                else reject("Cannot find lyrics");
+            });
+        }
     });
 }
 
@@ -23,21 +33,26 @@ function cleanString(str) {
 }
 
 function lyricsFromHTML(str) {
-    const dom = new jsdom.JSDOM(str);
-    const document = dom.window.document;
-    const p = document.querySelector(".mxm-lyrics__content");
-    p.nextElementSibling.firstElementChild.remove();
-    const text_p1 = p.firstElementChild.innerHTML;
-    const text_p2 = p.nextElementSibling.firstElementChild.firstElementChild.innerHTML;
-    const lyrics = (text_p1 + text_p2).split("\n\n").join("\n").split("\n");
-    return lyrics;
+    return new Promise((resolve, reject) => {
+        const dom = new jsdom.JSDOM(str);
+        const document = dom.window.document;
+        const p = document.querySelector(".mxm-lyrics__content");
+        if (p === null) {
+            reject("Cannot find lyrics");
+        }
+        p.nextElementSibling.firstElementChild.remove();
+        const text_p1 = p.firstElementChild.innerHTML;
+        const text_p2 = p.nextElementSibling.firstElementChild.firstElementChild.innerHTML;
+        const lyrics = (text_p1 + text_p2).split("\n\n").join("\n").split("\n");
+        resolve(lyrics);
+    });
 }
 
 function findInPage(link) {
     return new Promise((resolve, reject) => {
         fetch(link).then(res => {
             res.text().then(text => {
-                resolve(lyricsFromHTML(text));
+                lyricsFromHTML(text).then(resolve).catch(reject);
             }).catch(reject);
         }).catch(reject);
     });
